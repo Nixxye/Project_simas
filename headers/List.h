b608b495369dfc93f2be5c7f6062f68b1b87dcb9
isabelatulio <isabelatulio@alunos.utfpr.edu.br>
2023-05-06 14:48:19 -0300

    Update List.h

--- headers/List.h
+++ headers/List.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 template <class TYPE>
-class List //provavelmente ta tudo errado
+class List 
 {
 public:
     template <class TE>
@@ -28,20 +28,28 @@ private:
     Element<TYPE>* first;
     Element<TYPE>* last;
     int size;
-
 public:
     List():first(NULL),last(NULL),size (0) {}
     Element<TYPE>* get_first() {return first;}
     Element<TYPE>* get_last() {return last;}
     int get_size() {return size;}
-    void remove(TYPE* dt)
+    void remove(TYPE* dt);
+    void remove_front();
+    void clear();
+    void push(TYPE* dt);
+    ~List();
+};
+template <class TYPE>
+void List<TYPE>:: remove(TYPE* dt)
     {
         Element<TYPE>* pAux = first;
         Element<TYPE>* prev = NULL;
+
         while (pAux != last) 
         {
             if (pAux->get_data() == dt) 
             {
+                
                 if (pAux == first) {
                     first = pAux->get_next();
                 } 
@@ -53,17 +61,18 @@ public:
                 {
                     prev->set_next(pAux->get_next());
                 }
-                pAux->~Element();
                 delete (pAux);
                 size--;
 
             }
-
+           
             prev = pAux;
             pAux = pAux->get_next();
         }
+        printf ("\n\nSize :%d\n", size);
     }
-    void remove_front()
+template <class TYPE>
+void List<TYPE>::remove_front()
     {
         Element<TYPE>* pAux = first;
         pAux = first->get_next();
@@ -71,15 +80,18 @@ public:
         delete first;
         first = pAux;    
     }
-    void clear()
+template <class TYPE>
+void List<TYPE>::clear()
     {
         while (first != last)
         {
             remove_front();
         }
     }
-    void push(TYPE* dt)
-    {
+
+template <class TYPE>
+void List<TYPE>::push(TYPE* dt)
+     {
         if (dt)
         {
             Element<TYPE>* new_elem = new Element<TYPE>();
@@ -94,17 +106,12 @@ public:
                 last->set_next(new_elem);
                 last = new_elem;
             }
-            new_elem->set_next(NULL);
+            //new_elem->set_next(NULL);
             size++;
         }
     }
-    ~List()
-    {
-        //printf("%d", size);
-        while(first!=last)
-        {
-            remove_front();
-        }
-    }
-};
-
+template <class TYPE>
+List<TYPE>::~List()
+{
+    clear();    
+}
