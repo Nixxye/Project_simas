2af1d67bfd81fc6e98a2725b589c52a09d444dec
Nixxye <majornix051@gmail.com>
2023-05-09 19:39:38 -0300

    Update ColisionManager.cpp

--- managers/ColisionManager.cpp
+++ managers/ColisionManager.cpp
@@ -50,4 +50,23 @@ namespace Managers
             cout<<ypB-ysA<<" "<<ypB<<" "<< ypA<<endl;
         }
     }
+    /*
+    void ColisionManager::aux_col(Entity* A, Entity* B)
+    {
+        //VERTICAL COLISION:
+        //bellow the player
+        float xpA = A->get_position().x, ypA = A->get_position().y, xpB = B->get_position().x, ypB = B->get_position().y;
+        float xsA = A->get_size().x, ysA = A->get_size().y, xsB = B->get_size().x , ysB = B->get_size().y; 
+        if (ypA + ysA > ypB && ypA < ypB + ysB && ((xpA < xpB+xsB && xpA > xpB)||(xpA+xsA < xpB+xsB && xpA+xsA > xpB)||(xpA > xpB+xsB && xpA < xpB)))
+        {
+            A->set_position(sf::Vector2f(A->get_position().x, ypB-ysA));
+            A->set_vel(sf::Vector2f(A->get_vel().x, 0.f));
+            A->set_grounded(true);
+            //cout<<"EPAAAAA"<<endl;
+            A->troca_cor();
+            B->troca_cor();
+            cout<<ypB-ysA<<" "<<ypB<<" "<< ypA<<endl;
+        }
+    }
+    */
 }
