6e75b130f293b751610084b59173c0e86fd4362f
isabelatulio <isabelatulio@alunos.utfpr.edu.br>
2023-05-31 08:42:18 -0300

    testanto texturas

--- src/Obstacle.cpp
+++ src/Obstacle.cpp
@@ -7,7 +7,8 @@ namespace Entes
         Obstacle::Obstacle(sf::Vector2f pos, sf::Vector2f size):
         Entes::Entity(pos, sf::Vector2f(0.f, 0.f), size)
         {
-
+            texture = pGM->load_textures("../assets/obstacle.png");
+            body.setTexture(texture);
         }
         Obstacle::~Obstacle()
         {
